e3cede3c1a932c01ac4b34833cdd7b89cf0a0506
WallyS02 <sebkutny@gmail.com>
2024-08-07 11:40:52 +0200

    Fixed API calls proxying.

--- frontend/the_plug_svelte_frontend/src/service/config.ts
+++ frontend/the_plug_svelte_frontend/src/service/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = '/api/';
 
 export async function sendRequest(link: string, requestOptions: {}): Promise<any> {
     const response = await fetch(API_URL + link, requestOptions);
--- frontend/the_plug_svelte_frontend/vite.config.ts
+++ frontend/the_plug_svelte_frontend/vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { readFileSync } from 'fs'
 
-const proxyConfig = JSON.parse(readFileSync('src/proxy.conf.json', 'utf-8'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: proxyConfig,
+    proxy: {
+      '/api': {
+        'target': 'http://localhost:8080',
+        'secure': false,
+        'changeOrigin': true
+      }
+    },
     port: 80,
   },
-  plugins: [svelte()],
-  define: {
-    'process.env': process.env
-  }
+  plugins: [svelte()]
 })
