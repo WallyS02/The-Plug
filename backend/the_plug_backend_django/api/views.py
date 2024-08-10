6c698f452feb57974be9a24889d63eb20a466e2f
WallyS02 <sebkutny@gmail.com>
2024-08-10 21:17:28 +0200

    Updated Location list endpoint.

--- backend/the_plug_backend_django/api/views.py
+++ backend/the_plug_backend_django/api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-from rest_framework import generics, status
+from rest_framework import generics, status, serializers
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
@@ -63,7 +63,12 @@ class PlugRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
 
 class LocationList(generics.ListAPIView):
-    serializer_class = LocationSerializer
+    class LocationPlusPlugUsernameAndRatingSerializer(LocationSerializer):
+        username = serializers.CharField()
+        rating = serializers.FloatField()
+        offered_drugs = serializers.ListField(child=serializers.CharField())
+
+    serializer_class = LocationPlusPlugUsernameAndRatingSerializer
 
     def get_queryset(self):
         north = self.request.query_params.get('north')
@@ -71,6 +76,8 @@ class LocationList(generics.ListAPIView):
         east = self.request.query_params.get('east')
         west = self.request.query_params.get('west')
         plug_id = self.request.query_params.get('plug')
+        drugs = self.request.query_params.getlist('drugs')
+        plugs = self.request.query_params.getlist('plugs')
 
         if not all([north, south, east, west]):
             return Location.objects.none()
@@ -83,24 +90,57 @@ class LocationList(generics.ListAPIView):
         except ValueError:
             return Location.objects.none()
 
+        class LocationPlusPlugUsernameAndRating(Location):
+            username = str()
+            rating = float()
+            offered_drugs = []
+
+        def map_to_id(drug_offer):
+            return drug_offer.drug_id
+
+        q = Q()
+
+        q &= Q(latitude__lte=north)
+        q &= Q(latitude__gte=south)
+        q &= Q(longitude__lte=west)
+        q &= Q(longitude__gte=east)
+
         if plug_id != '':
             plug = get_object_or_404(Plug, pk=plug_id)
 
-            return Location.objects.filter(
-                ~Q(plug=plug),
-                latitude__lte=north,
-                latitude__gte=south,
-                longitude__lte=west,
-                longitude__gte=east
-            )
+            q &= ~Q(plug=plug)
 
-        else:
-            return Location.objects.filter(
-                latitude__lte=north,
-                latitude__gte=south,
-                longitude__lte=west,
-                longitude__gte=east
-            )
+        if drugs:
+            q &= Q(plug__drugoffer__drug__id__in=drugs)
+
+        if plugs:
+            q &= Q(plug__in=plugs)
+
+        locations = Location.objects.filter(q).distinct()
+
+        locations_plus_plug_usernames_and_ratings = []
+
+        for location in locations:
+            location_plus_plug_username_and_rating = LocationPlusPlugUsernameAndRating()
+            location_plus_plug_username_and_rating.id = location.id
+            location_plus_plug_username_and_rating.longitude = location.longitude
+            location_plus_plug_username_and_rating.latitude = location.latitude
+            location_plus_plug_username_and_rating.street_name = location.street_name
+            location_plus_plug_username_and_rating.street_number = location.street_number
+            location_plus_plug_username_and_rating.city = location.city
+            location_plus_plug_username_and_rating.plug = location.plug
+            location_plus_plug_username_and_rating.username = location.plug.app_user.username
+            location_plus_plug_username_and_rating.rating = location.plug.rating
+
+            drug_offers = DrugOffer.objects.filter(plug=location.plug)
+            drug_offers = list(map(map_to_id, drug_offers))
+            drugs = Drug.objects.filter(id__in=drug_offers)
+
+            location_plus_plug_username_and_rating.offered_drugs = drugs
+
+            locations_plus_plug_usernames_and_ratings.append(location_plus_plug_username_and_rating)
+
+        return locations_plus_plug_usernames_and_ratings
 
 
 @authentication_classes([SessionAuthentication, TokenAuthentication])
@@ -184,8 +224,6 @@ class DrugOfferRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
 
 
-@authentication_classes([SessionAuthentication, TokenAuthentication])
-@permission_classes([IsAuthenticated])
 class DrugList(generics.ListAPIView):
     queryset = Drug.objects.all()
     serializer_class = DrugSerializer
