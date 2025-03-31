f521b2a3bc11aa3e69919109f7e141fcfed3fc3f
WallyS02 <sebkutny@gmail.com>
2025-03-31 13:47:13 +0200

    Added remote backend state storage.

--- aws/iac/versions.tf
+++ aws/iac/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  cloud {
+    organization = "The-Plug"
+
+    workspaces {
+      name = "The-Plug-Workspace"
+    }
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
