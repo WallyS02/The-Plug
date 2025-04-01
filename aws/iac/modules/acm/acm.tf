13c9bf9a0b5f5ca43ced622a7f798153cc280f22
WallyS02 <sebkutny@gmail.com>
2025-04-01 14:35:33 +0200

    Added first, untested version of ACM module

---
+++ aws/iac/modules/acm/acm.tf
@@ -0,0 +1,34 @@
+# Certificate creation
+resource "aws_acm_certificate" "certificate" {
+  domain_name               = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+  validation_method         = var.validation_method
+  tags                      = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Route 53 DNS verification
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = var.hosted_zone_id
+  ttl     = 60
+  records = [each.value.record]
+}
+
+# Certificate approval
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+}
